422802a9cf7f7275218710867f1960bff4139325
Jason Volk <jason@zemos.net>
2025-12-21 13:08:48 +0000

    Add serde_html_form to Error; use serde_core for greater abstraction.
    Signed-off-by: Jason Volk <jason@zemos.net>

--- src/core/error/serde.rs
+++ src/core/error/serde.rs
@@ -1,13 +1,11 @@
 use std::fmt::Display;
 
-use serde::{de, ser};
-
 use crate::Error;
 
-impl de::Error for Error {
+impl serde_core::de::Error for Error {
 	fn custom<T: Display + ToString>(msg: T) -> Self { Self::SerdeDe(msg.to_string().into()) }
 }
 
-impl ser::Error for Error {
+impl serde_core::ser::Error for Error {
 	fn custom<T: Display + ToString>(msg: T) -> Self { Self::SerdeSer(msg.to_string().into()) }
 }
