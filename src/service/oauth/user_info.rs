5bec37ba5b74ee180580f4aff13ff04880542ccc
stevessr <steve-ssr@outlook.com>
2026-01-07 00:11:59 +0800

    fix

--- src/service/oauth/user_info.rs
+++ src/service/oauth/user_info.rs
@@ -1,7 +1,10 @@
-use serde::{Deserialize, Serialize};
+use serde::{
+	Deserialize, Serialize,
+	de::{self, Deserializer},
+};
 
 /// Selection of userinfo response claims.
-#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[derive(Clone, Debug, Default, Serialize)]
 pub struct UserInfo {
 	/// Unique identifier number or login username. Usually a number on most
 	/// services. We consider a concatenation of the `iss` and `sub` to be a
@@ -10,7 +13,6 @@ pub struct UserInfo {
 	///
 	/// Considered for user mxid only if none of the better fields are defined.
 	/// `login` alias intended for github.
-	#[serde(alias = "login")]
 	pub sub: String,
 
 	/// The login username we first consider when defined.
@@ -36,4 +38,44 @@ pub struct UserInfo {
 
 	/// URL to pfp (google)
 	pub picture: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct RawUserInfo {
+	sub: Option<String>,
+	#[serde(rename = "login")]
+	login: Option<String>,
+	preferred_username: Option<String>,
+	nickname: Option<String>,
+	name: Option<String>,
+	given_name: Option<String>,
+	family_name: Option<String>,
+	email: Option<String>,
+	avatar_url: Option<String>,
+	picture: Option<String>,
+}
+
+impl<'de> Deserialize<'de> for UserInfo {
+	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+	where
+		D: Deserializer<'de>,
+	{
+		let raw = RawUserInfo::deserialize(deserializer)?;
+		let sub = raw
+			.sub
+			.or(raw.login)
+			.ok_or_else(|| de::Error::missing_field("sub"))?;
+
+		Ok(UserInfo {
+			sub,
+			preferred_username: raw.preferred_username,
+			nickname: raw.nickname,
+			name: raw.name,
+			given_name: raw.given_name,
+			family_name: raw.family_name,
+			email: raw.email,
+			avatar_url: raw.avatar_url,
+			picture: raw.picture,
+		})
+	}
 }
