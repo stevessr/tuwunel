7b849f2762b980931cb0c671e4dd1c7bae12ac33
Jason Volk <jason@zemos.net>
2026-03-24 21:48:40 +0000

    Add main integration test for admin execute echo.
    Signed-off-by: Jason Volk <jason@zemos.net>

---
+++ src/main/tests/admin_execute_echo.rs
@@ -0,0 +1,27 @@
+#![cfg(test)]
+#![allow(unused_features)] // 1.96.0-nightly 2026-03-07 bug
+
+use insta::{assert_debug_snapshot, with_settings};
+use tuwunel::{Args, Server, runtime};
+use tuwunel_core::Result;
+
+#[test]
+fn admin_execute_echo() -> Result {
+	with_settings!({
+		description => "Admin Execute Echo",
+		snapshot_suffix => "admin_execute_echo",
+	}, {
+		let mut args = Args::default_test(&["smoke", "fresh", "cleanup"]);
+		args.execute.push("debug echo Test".into());
+
+		let runtime = runtime::new(Some(&args))?;
+		let server = Server::new(Some(&args), Some(runtime.handle()))?;
+		let result = runtime.block_on(async {
+			tuwunel::async_exec(&server).await
+		});
+
+		runtime::shutdown(&server, runtime)?;
+		assert_debug_snapshot!(result);
+		result
+	})
+}
