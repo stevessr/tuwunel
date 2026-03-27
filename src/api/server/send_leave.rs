96e5fdff379beb740e6d79593ecf1f1fc64405af
dasha_uwu <dasha@linuxping.win>
2026-03-27 06:57:48 +0500

    Remove /v1/send_{join,leave} per MSC4376 [v1.18]

--- src/api/server/send_leave.rs
+++ src/api/server/send_leave.rs
@@ -1,32 +1,16 @@
-#![expect(deprecated)]
-
 use axum::extract::State;
 use futures::FutureExt;
 use ruma::{
-	OwnedRoomId, OwnedUserId, RoomId, ServerName,
+	OwnedRoomId, OwnedUserId,
 	api::federation::membership::create_leave_event,
 	events::{
 		StateEventType,
 		room::member::{MembershipState, RoomMemberEventContent},
 	},
 };
-use serde_json::value::RawValue as RawJsonValue;
 use tuwunel_core::{Err, Result, at, err, matrix::event::gen_event_id_canonical_json};
-use tuwunel_service::Services;
 
 use crate::Ruma;
-
-/// # `PUT /_matrix/federation/v1/send_leave/{roomId}/{eventId}`
-///
-/// Submits a signed leave event.
-pub(crate) async fn create_leave_event_v1_route(
-	State(services): State<crate::State>,
-	body: Ruma<create_leave_event::v1::Request>,
-) -> Result<create_leave_event::v1::Response> {
-	create_leave_event(&services, body.origin(), &body.room_id, &body.pdu).await?;
-
-	Ok(create_leave_event::v1::Response::new())
-}
 
 /// # `PUT /_matrix/federation/v2/send_leave/{roomId}/{eventId}`
 ///
@@ -35,17 +19,9 @@ pub(crate) async fn create_leave_event_v2_route(
 	State(services): State<crate::State>,
 	body: Ruma<create_leave_event::v2::Request>,
 ) -> Result<create_leave_event::v2::Response> {
-	create_leave_event(&services, body.origin(), &body.room_id, &body.pdu).await?;
+	let room_id = &body.room_id;
+	let origin = body.origin();
 
-	Ok(create_leave_event::v2::Response::new())
-}
-
-async fn create_leave_event(
-	services: &Services,
-	origin: &ServerName,
-	room_id: &RoomId,
-	pdu: &RawJsonValue,
-) -> Result {
 	if !services.metadata.exists(room_id).await {
 		return Err!(Request(NotFound("Room is unknown to this server.")));
 	}
@@ -59,7 +35,7 @@ async fn create_leave_event(
 	// We do not add the event_id field to the pdu here because of signature and
 	// hashes checks
 	let room_version_id = services.state.get_room_version(room_id).await?;
-	let Ok((event_id, value)) = gen_event_id_canonical_json(pdu, &room_version_id) else {
+	let Ok((event_id, value)) = gen_event_id_canonical_json(&body.pdu, &room_version_id) else {
 		// Event could not be converted to canonical json
 		return Err!(Request(BadJson("Could not convert event to canonical json.")));
 	};
@@ -74,7 +50,7 @@ async fn create_leave_event(
 	)
 	.map_err(|e| err!(Request(BadJson(warn!("room_id field is not a valid room ID: {e}")))))?;
 
-	if event_room_id != room_id {
+	if event_room_id != *room_id {
 		return Err!(Request(BadJson("Event room_id does not match request path room ID.")));
 	}
 
@@ -160,5 +136,7 @@ async fn create_leave_event(
 		.sending
 		.send_pdu_room(room_id, &pdu_id)
 		.boxed()
-		.await
+		.await?;
+
+	Ok(create_leave_event::v2::Response::new())
 }
