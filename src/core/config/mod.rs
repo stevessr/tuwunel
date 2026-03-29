ab758f89069ad0a6760f3e222abb9f5f8a0abfa2
Jason Volk <jason@zemos.net>
2026-03-29 06:47:02 +0000

    Move identity_provider_serde helper into separate unit.
    Signed-off-by: Jason Volk <jason@zemos.net>

--- src/core/config/mod.rs
+++ src/core/config/mod.rs
@@ -1,4 +1,5 @@
 pub mod check;
+mod identity_provider_serde;
 pub mod manager;
 pub mod proxy;
 pub mod room_version;
@@ -2907,52 +2908,6 @@ impl IdentityProvider {
 		let client_secret = tokio::fs::read_to_string(client_secret_file).await?;
 
 		Ok(client_secret.trim().to_owned())
-	}
-}
-
-mod identity_provider_serde {
-	use std::{collections::BTreeMap, fmt, marker::PhantomData};
-
-	use serde::{
-		Deserializer, de,
-		de::{MapAccess, SeqAccess},
-	};
-
-	struct Visitor(PhantomData<IdentityProviders>);
-
-	type IdentityProviders = BTreeMap<String, super::IdentityProvider>;
-
-	pub(super) fn deserialize<'de, D>(de: D) -> Result<IdentityProviders, D::Error>
-	where
-		D: Deserializer<'de>,
-	{
-		de.deserialize_any(Visitor(PhantomData))
-	}
-
-	impl<'de> de::Visitor<'de> for Visitor {
-		type Value = IdentityProviders;
-
-		fn expecting(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
-			fmt.write_str("Mapping or Sequence")
-		}
-
-		fn visit_map<A: MapAccess<'de>>(self, mut map: A) -> Result<Self::Value, A::Error> {
-			let mut ret = Self::Value::new();
-			while let Some((k, v)) = map.next_entry()? {
-				ret.insert(k, v);
-			}
-
-			Ok(ret)
-		}
-
-		fn visit_seq<A: SeqAccess<'de>>(self, mut seq: A) -> Result<Self::Value, A::Error> {
-			let mut ret = Self::Value::new();
-			while let Some(v) = seq.next_element()? {
-				ret.insert(ret.len().to_string(), v);
-			}
-
-			Ok(ret)
-		}
 	}
 }
 
