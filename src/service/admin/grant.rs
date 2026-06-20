e5d62af2eb9f0f24b852be29890729d162db0963
June Strawberry <june@girlboss.ceo>
2026-06-20 12:25:42 -0400

    grant power levels when the target is already in the admin room (#84)
    Signed-off-by: June Strawberry <june@girlboss.ceo>

--- src/service/admin/grant.rs
+++ src/service/admin/grant.rs
@@ -1,6 +1,6 @@
 use futures::FutureExt;
 use ruma::{
-	UserId,
+	RoomId, UserId,
 	events::{
 		StateEventType,
 		room::{
@@ -13,6 +13,8 @@ use ruma::{
 use tuwunel_core::{
 	Err, Result, debug_info, debug_warn, error, implement, matrix::pdu::PduBuilder,
 };
+
+use crate::rooms::state::RoomMutexGuard;
 
 /// Invite the user to the tuwunel admin room.
 ///
@@ -28,25 +30,108 @@ pub async fn make_user_admin(&self, user_id: &UserId) -> Result {
 
 	let state_lock = self.services.state.mutex.lock(&room_id).await;
 
-	if self
+	let is_joined = self
 		.services
 		.state_cache
 		.is_joined(user_id, &room_id)
-		.await
-	{
-		return Err!(debug_warn!("User is already joined in the admin room"));
-	}
+		.await;
 
-	if self
+	let is_invited = self
 		.services
 		.state_cache
 		.is_invited(user_id, &room_id)
-		.await
-	{
-		return Err!(debug_warn!("User is already pending an invitation to the admin room"));
+		.await;
+
+	let already_member = is_joined || is_invited;
+
+	if !already_member {
+		self.invite_new_admin(user_id, &room_id, &state_lock)
+			.await?;
 	}
 
-	// Use the server user to grant the new admin's power level
+	let server_user: &UserId = self.services.globals.server_user.as_ref();
+
+	let mut room_power_levels = self
+		.services
+		.state_accessor
+		.room_state_get_content::<RoomPowerLevelsEventContent>(
+			&room_id,
+			&StateEventType::RoomPowerLevels,
+			"",
+		)
+		.await
+		.unwrap_or_default();
+
+	let server_level = 69420.into();
+	let admin_level = 100.into();
+	let already_granted = room_power_levels.users.get(server_user) == Some(&server_level)
+		&& room_power_levels.users.get(user_id) == Some(&admin_level);
+
+	if !already_granted {
+		room_power_levels
+			.users
+			.insert(server_user.into(), server_level);
+		room_power_levels
+			.users
+			.insert(user_id.into(), admin_level);
+
+		self.services
+			.timeline
+			.build_and_append_pdu(
+				PduBuilder::state(String::new(), &room_power_levels),
+				server_user,
+				&room_id,
+				&state_lock,
+			)
+			.await?;
+	}
+
+	// Set room tag
+	let room_tag = self
+		.services
+		.server
+		.config
+		.admin_room_tag
+		.as_str();
+
+	if !already_granted
+		&& !room_tag.is_empty()
+		&& let Err(e) = self
+			.services
+			.account_data
+			.set_room_tag(user_id, &room_id, room_tag.into(), None)
+			.await
+	{
+		error!(?room_id, ?user_id, ?room_tag, "Failed to set tag for admin grant: {e}");
+	}
+
+	if !already_member && self.services.server.config.admin_room_notices {
+		let welcome_message = String::from(
+			"## Thank you for trying out tuwunel!\n\nTuwunel is a continuation of conduwuit which was technically a hard fork of Conduit.\n\nHelpful links:\n> GitHub Repo: https://github.com/matrix-construct/tuwunel\n> Documentation: https://matrix-construct.github.io/tuwunel\n> Report issues: https://github.com/matrix-construct/tuwunel/issues\n\nFor a list of available commands, send the following message in this room: `!admin --help`",
+		);
+
+		// Send welcome message
+		self.services
+			.timeline
+			.build_and_append_pdu(
+				PduBuilder::timeline(&RoomMessageEventContent::text_markdown(welcome_message)),
+				server_user,
+				&room_id,
+				&state_lock,
+			)
+			.await?;
+	}
+
+	Ok(())
+}
+
+#[implement(super::Service)]
+async fn invite_new_admin(
+	&self,
+	user_id: &UserId,
+	room_id: &RoomId,
+	state_lock: &RoomMutexGuard,
+) -> Result {
 	let server_user = self.services.globals.server_user.as_ref();
 
 	// if this is our local user, just forcefully join them in the room. otherwise,
@@ -61,8 +146,8 @@ pub async fn make_user_admin(&self, user_id: &UserId) -> Result {
 					&RoomMemberEventContent::new(MembershipState::Invite),
 				),
 				server_user,
-				&room_id,
-				&state_lock,
+				room_id,
+				state_lock,
 			)
 			.await?;
 
@@ -75,8 +160,8 @@ pub async fn make_user_admin(&self, user_id: &UserId) -> Result {
 					&RoomMemberEventContent::new(MembershipState::Join),
 				),
 				user_id,
-				&room_id,
-				&state_lock,
+				room_id,
+				state_lock,
 			)
 			.await?;
 	} else {
@@ -89,72 +174,8 @@ pub async fn make_user_admin(&self, user_id: &UserId) -> Result {
 					&RoomMemberEventContent::new(MembershipState::Invite),
 				),
 				server_user,
-				&room_id,
-				&state_lock,
-			)
-			.await?;
-	}
-
-	// Set power levels
-	let mut room_power_levels = self
-		.services
-		.state_accessor
-		.room_state_get_content::<RoomPowerLevelsEventContent>(
-			&room_id,
-			&StateEventType::RoomPowerLevels,
-			"",
-		)
-		.await
-		.unwrap_or_default();
-
-	room_power_levels
-		.users
-		.insert(server_user.into(), 69420.into());
-	room_power_levels
-		.users
-		.insert(user_id.into(), 100.into());
-
-	self.services
-		.timeline
-		.build_and_append_pdu(
-			PduBuilder::state(String::new(), &room_power_levels),
-			server_user,
-			&room_id,
-			&state_lock,
-		)
-		.await?;
-
-	// Set room tag
-	let room_tag = self
-		.services
-		.server
-		.config
-		.admin_room_tag
-		.as_str();
-
-	if !room_tag.is_empty()
-		&& let Err(e) = self
-			.services
-			.account_data
-			.set_room_tag(user_id, &room_id, room_tag.into(), None)
-			.await
-	{
-		error!(?room_id, ?user_id, ?room_tag, "Failed to set tag for admin grant: {e}");
-	}
-
-	if self.services.server.config.admin_room_notices {
-		let welcome_message = String::from(
-			"## Thank you for trying out tuwunel!\n\nTuwunel is a continuation of conduwuit which was technically a hard fork of Conduit.\n\nHelpful links:\n> GitHub Repo: https://github.com/matrix-construct/tuwunel\n> Documentation: https://matrix-construct.github.io/tuwunel\n> Report issues: https://github.com/matrix-construct/tuwunel/issues\n\nFor a list of available commands, send the following message in this room: `!admin --help`",
-		);
-
-		// Send welcome message
-		self.services
-			.timeline
-			.build_and_append_pdu(
-				PduBuilder::timeline(&RoomMessageEventContent::text_markdown(welcome_message)),
-				server_user,
-				&room_id,
-				&state_lock,
+				room_id,
+				state_lock,
 			)
 			.await?;
 	}
